e3c6632ebb46b57347f7c35cd82f65eabf058544
Logan <lbrenneis21@gmail.com>
2026-08-29 09:52:45 -0600

    waybar updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,13 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
     priority = 1000, -- Make sure it loads first
     config = function()
       -- Optional configuration
-      require("gruvbox").setup({
-        terminal_colors = true, -- Add gruvbox colors to the :terminal
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, current line
-        contrast = "medium", -- can be "hard", "medium" or "soft"
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme gruvbox")
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd("colorscheme gruvbox-material")
     end,
   },
 }
